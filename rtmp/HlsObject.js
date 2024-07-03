f830971c10ec6f215fa52eef8106dc32cafc6695
zhivkojujo <zhivkojujo@gmail.com>
2024-07-03 22:54:08 +0300

    class for Hls creation

--- rtmp/HlsObject.js
+++ rtmp/HlsObject.js
@@ -3,10 +3,11 @@ export default class HlsObject {
     constructor(name, video) {
         this.name = name;
         this.video = video;
+        this.hls = null;
     }
 
     create() {
-        const hls = new Hls({
+        this.hls = new Hls({
             liveSyncDuration: 4, // Sync to the live edge within 3 seconds
             liveMaxLatencyDuration: 5, // Max latency duration for live streams
             maxLiveSyncPlaybackRate: 1.5, // Faster catch-up when behind live edge
@@ -17,10 +18,28 @@ export default class HlsObject {
             backBufferLength: 30
         });
 
-        hls.loadSource(`/hls/${this.name}.m3u8`); // Replace with actual M3U8 stream URL
-        hls.attachMedia(this.video);
-        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        this.hls.loadSource(`/hls/${this.name}.m3u8`); // Replace with actual M3U8 stream URL
+        this.hls.attachMedia(this.video);
+        this.hls.on(Hls.Events.MANIFEST_PARSED, function () {
             this.video.play();
         });
+    }
+
+    pause() {
+        if (this.hls) {
+            this.hls.stopLoad();
+        }
+    }
+
+    resume() {
+        if (this.hls) {
+            this.hls.startLoad();
+        }
+    }
+
+    destroy() {
+        if (this.hls) {
+            this.hls.destroy()
+        }
     }
 }
